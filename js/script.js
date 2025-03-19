8726b97bd0f56b379e5765bf7297942b453380cc
Engr. Boncoeur Innocent <boncoeurinnocent@outlook.com>
2025-03-19 07:51:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
-
 $(document).ready(function(){
-    console.log("jQuery Loaded");
-    
-    if ($.fn.slick) {
-        console.log("Slick Loaded Successfully");
-    } else {
-        console.log("Slick Failed to Load");
-    }
-
     $('.photo-slider').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        arrows: false,
-        fade: false,
-        infinite: true
+        vertical: false,          // Enable vertical slide
+        infinite: true,          // Infinite scroll
+        speed: 500,              // Speed of sliding
+        slidesToShow: 1,         // Show one image at a time
+        slidesToScroll: 1,       // Scroll one image at a time
+        autoplay: true,          // Enable auto slide
+        autoplaySpeed: 3000,     // Speed of auto slide in ms
+        arrows: true,           // Disable arrows if not needed
+        dots: false,              // Enable dots for navigation
+        adaptiveHeight: true     // Adapt height of each slide
     });
 });
